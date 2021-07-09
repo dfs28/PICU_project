1cef3d2771547c86af9f95671935db7359eb406d
dfs28 <danwone1@googlemail.com>
2021-07-09 16:50:26 +0100

    Trying to fix access problem

--- scripts/Pandas_to_np.py
+++ scripts/Pandas_to_np.py
@@ -8,10 +8,22 @@ import re
 flowsheet = pd.read_csv('~/Project/Project_data/files/flowsheet_output.csv', parse_dates = ['taken_datetime'])
 
 #Sort out the problem with MAP, make sure any lost go to BP etc 
-mapped = list(map(type, flowsheet['MAP']))
-slash = re.compile('[\D]+')
-strings = [i for i, j in enumerate(mapped) if j == str]
-flowsheet.loc[strings, 'MAP']
+#Pull out where MAP is a **/** format
+slash = re.compile('.*/+.*')
+strings = [i for i, j in enumerate(flowsheet['MAP']) if not slash.match(str(j)) == None]
+strings = np.array(strings)
+
+#Find the location where sysbp and diabp are missing but map not
+diaNAs = np.where(flowsheet['DiaBP'].isna())
+sysNAs = np.where(flowsheet['SysBP'].isna())
+BP_NAs = np.union1d(diaNAs, sysNAs)
+MAPnotBP = np.intersect1d(BP_NAs, strings)
+#Can see actually none here that are relevant
+
+#Now just fix the problem for the MAPs that are strings
+newBPs = flowsheet.loc[strings, 'MAP'].str.split("/", n = 1, expand = True)
+newBPs = newBPs.astype('float')
+flowsheet.loc[strings, 'MAP'] = newBPs.loc[:, 0]*(1/3) + newBPs.loc[:, 1]*(2/3)
 
 #Make a unique patients column
 unique_patients 
