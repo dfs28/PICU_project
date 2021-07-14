lots of fixes, scaling to percentile, reporting splines

scripts/Pandas_to_np.py
@@ -6,11 +6,12 @@ import re
 import matplotlib.pyplot as plt
 import math
 from datetime import datetime
+from scipy import stats
+import scipy
 
 #In reality import the big flowsheet to use thing
 flowsheet = pd.read_csv('/store/DAMTP/dfs28/PICU_data/flowsheet_zscores.csv', parse_dates = ['taken_datetime'])
 print('Flowsheet loaded: ', datetime.now().strftime("%H:%M:%S"))
-
 
 #Fix issue with ethnicity, sex and died
 died = {'N': 0, 'Y': 1}
@@ -22,18 +23,24 @@ flowsheet['ethnicity'] = flowsheet['ethnicity'].replace(ethnicity)
 print('Correction to float applied: ', datetime.now().strftime("%H:%M:%S"))
 
 #Make 3d training array
-def make_3d_array(array, length, all_cols, point_cols, series_cols):
+def make_3d_array(array, length, all_cols, point_cols, series_cols, percentile = True):
     """
     Function to make an pandas into a 3d np.array of slices and stack them \n
     Takes a dataframe, slices it by the length specified 
     Expects the time series to be longer than the number of vars
     Specify the length
+    Percentile scales by rank - this should mean that fluctuations within the normal range are still visible
     """
 
     #Get the shape, work out what shape the new array should be
-    array_use = np.array(array.loc[:, all_cols])
-    i_point_cols = [i for i, j in enumerate(all_cols) if j in point_cols]
-    i_series_cols = [i for i, j in enumerate(all_cols) if j in series_cols]
+    all_used_cols = [i for i in array.columns if i in all_cols]
+    print(all_used_cols)
+
+    array_use = np.array(array.loc[:, all_used_cols])
+    i_point_cols = [i for i, j in enumerate(all_used_cols) if j in point_cols]
+    j_point_cols = [j for i, j in enumerate(all_used_cols) if j in point_cols]
+    i_series_cols = [i for i, j in enumerate(all_used_cols) if j in series_cols]
+    j_series_cols = [j for i, j in enumerate(all_used_cols) if j in series_cols]
 
     #Make this an np array with no nans
     for i in range(array_use.shape[1]):
@@ -41,16 +48,20 @@ def make_3d_array(array, length, all_cols, point_cols, series_cols):
         if sum(nas == False) < 1:
             print(i)
             continue
+
+        #Scale to percentile
+        if percentile:
+            array_use[nas == False, i] = stats.rankdata(array_use[nas == False, i])
+
         min = np.min(array_use[nas == False, i])
         max = np.max(array_use[nas == False, i])
         array_use[nas, i] = min
 
-        #Now normalise
+        #Now normalise 0-1
         array_use[:, i] -= min
         if not (max - min) == 0:
             array_use[:, i] /= (max - min) 
 
-        #Could also consider then normalising by centile/ alternatively normalising by centile
         
     shape = array_use.shape
     z_dim = math.floor(np.max(shape)/length)
@@ -75,9 +86,10 @@ def make_3d_array(array, length, all_cols, point_cols, series_cols):
 
     x_dim = length
     y_dim = np.min(shape)
-    array_3d = np.empty((len(to_use), len(series_cols), x_dim))
-    array_2d = np.empty((len(to_use), len(point_cols)))
-    array_characteristics = np.empty((len(to_use), len(series_cols)*2))
+    array_3d = np.empty((len(to_use), len(i_series_cols), x_dim))
+    array_2d = np.empty((len(to_use), len(i_point_cols)))
+    array_characteristics = np.empty((len(to_use), len(i_series_cols)*2))
+    splines = np.empty((len(to_use), 8*len(i_series_cols)))
 
     #Outcomes
     outcomes = np.zeros((len(to_use), 12))
@@ -114,6 +126,8 @@ def make_3d_array(array, length, all_cols, point_cols, series_cols):
         discharge_date = all_dates[all_dates.index[-1]]
         time_to_discharge = discharge_date - end_of_section
 
+        #Correct for death prior to discharge
+
         #Now assign depending on how soon:
         if time_to_discharge < np.timedelta64(2, 'D'):
             outcomes[position, 5] = 1
@@ -149,10 +163,17 @@ def make_3d_array(array, length, all_cols, point_cols, series_cols):
         else:
             outcomes[position, 11] = array.loc[end_position, 'time_to_death']
 
-        ##Now get pointwise variables
-        medians = [np.median(temp_array[:, i]) for i in range(np.shape(temp_array)[1])]
+        ##Now get pointwise variables - could do median and mad depending on how scale to percentile looks?
+        means = [np.mean(temp_array[:, i]) for i in range(np.shape(temp_array)[1])]
         st_devs = [np.std(temp_array[:, i]) for i in range(np.shape(temp_array)[1])]
-        array_characteristics[position, :] = np.array(medians + st_devs)
+        array_characteristics[position, :] = np.array(means + st_devs)
+        
+        ##Fit splines to approximate curve
+        for i in range(temp_array.shape[1]):
+
+            #Get polynomial values, smoothing seems to make this all the same length (8, will need to make this adaptable if change length)
+            polynomials = scipy.interpolate.splrep(x = range(len(temp_array[:, i])), y = temp_array[:, i], s= 100)[1]
+            splines[position, range(i*8, (i+1)*8)] = polynomials
         
 
     
@@ -163,26 +184,18 @@ def make_3d_array(array, length, all_cols, point_cols, series_cols):
     na_loc_char = np.isnan(array_characteristics)
     array_characteristics[na_loc_char] = 0
 
-    return array_3d, array_2d, array_characteristics, outcomes, slicesPerPatient
+    return array_3d, array_2d, array_characteristics, splines, outcomes, slicesPerPatient, all_used_cols, j_point_cols, j_series_cols
 
-point_cols = ['ALT', 'Albumin', 'AlkPhos','AST', 'Aspartate', 'Amylase', 'APTT', 'Anion_gap', 'Base_excess', 'Basophils', 'Bicarb',
-            'pH', 'Blood_culture', 'CRP', 'Ca2+', 'Cl', 'Eosinophils', 'FHHb', 'FMetHb', 'FO2Hb', 'Glucose', 'HCT%', 'HCT', 'INR',
-            'Lactate', 'Lymphs', 'Mg', 'Monocytes', 'Neuts', 'P50', 'PaCO2', 'PcCO2', 'PmCO2', 'PaO2', 'PcO2', 'PmO2', 'PO2', 
-            'PvCO2', 'PcO2.1', 'Phos', 'Plts', 'K+', 'PT', 'Retics', 'Na+', 'TT', 'Bili', 'WCC', 'Strong_ion_gap', 'Age_yrs', 'sex', 
-            'ethnicity', 'interpolated_wt_kg']
-
-""" Need to fix issue with childsds
 point_cols = ['ALT', 'Albumin', 'AlkPhos','AST', 'Aspartate', 'Amylase', 'APTT', 'Anion_gap', 'Base_excess', 'Basophils', 'Bicarb',
             'pH', 'Blood_culture', 'CRP', 'Ca2+', 'Cl', 'Eosinophils', 'FHHb', 'FMetHb', 'FO2Hb', 'Glucose', 'HCT%', 'HCT', 'INR',
             'Lactate', 'Lymphs', 'Mg', 'Monocytes', 'Neuts', 'P50', 'PaCO2', 'PcCO2', 'PmCO2', 'PaO2', 'PcO2', 'PmO2', 'PO2', 
             'PvCO2', 'PcO2.1', 'Phos', 'Plts', 'K+', 'PT', 'Retics', 'Na+', 'TT', 'Bili', 'WCC', 'Strong_ion_gap', 'Age_yrs', 'sex', 
             'ethnicity', 'Weight_z_scores']
-            """
 
 series_cols =  ['Ventilation', 'HFO', 'IPAP', 'EPAP', 'Tracheostomy', 'ETCO2', 'FiO2', 'O2Flow/kg', 'Ventilation_L_min', 'Ventilation(ml)',
                 'MeanAirwayPressure', 'Ventilation_missing', 'O2Flow/kg_missing', 'IPAP_missing', 'EPAP_missing', 'FiO2_missing', 'HFO_missing',
                 'Tracheostomy_missing', 'Ventilation(ml)_missing', 'MeanAirwayPressure_missing', 'ETCO2_missing', 'SBP_zscore', 'DBP_zscore', 
-                'MAP_zscore', 'SysBP_missing', 'DiaBP_missing', 'HR_zscore', 'HR_missing', 'Comfort:Alertness', 'Comfort:BP', 'Comfort:Calmness',
+                'MAP_zscore', 'SysBP_missing', 'DiaBP_missing', 'MAP_missing', 'HR_zscore', 'HR_missing', 'Comfort:Alertness', 'Comfort:BP', 'Comfort:Calmness',
                 'Comfort', 'Comfort:HR', 'Comfort:Resp', 'AVPU', 'GCS_V', 'GCS_E', 'GCS_M', 'GCS', 'GCS_missing', 'AVPU_missing', 'CRT', 'CRT_missing',
                 'SpO2', 'SpO2_missing', 'interpolated_ht_m', 'ECMO', 'ECMO_missing', 'Inotropes_kg', 'Inotropes_missing', 'RR_zscore', 'RR_missing',
                 'dialysis', 'dialysis_missing', 'Temp', 'Temp_missing', 'Urine_output_kg', 'Urine_output_missing_kg', 'PEWS']
@@ -192,9 +205,10 @@ other_cols = ['time_to_death', 'died']
 all_cols = point_cols + series_cols + other_cols
 
 #Now run it
-array3d, array2d, array_characteristics, outcomes, slicesPerPatient = make_3d_array(flowsheet, 180, all_cols, point_cols, series_cols)
+array3d, array2d, array_characteristics, splines, outcomes, slicesPerPatient, all_used_cols, j_point_cols, j_series_cols = make_3d_array(flowsheet, 180, all_cols, point_cols, series_cols)
 print('Slicing done: ', datetime.now().strftime("%H:%M:%S"))
-np.savez('/store/DAMTP/dfs28/PICU_data/np_arrays.npz', d3 = array3d, d2 = array2d, chars = array_characteristics, outcomes = outcomes, per_pt = slicesPerPatient)
+np.savez('/store/DAMTP/dfs28/PICU_data/np_arrays.npz', d3 = array3d, d2 = array2d, chars = array_characteristics, splines = splines, 
+         outcomes = outcomes, per_pt = slicesPerPatient, all_used = np.array(all_used_cols), point_cols = np.array(j_point_cols), series_cols = np.array(j_series_cols))
 print('Saved: ', datetime.now().strftime("%H:%M:%S"))
 
 #Plot PEWS to decide where to set the cutoff