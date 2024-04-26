27887346fc091ce4d6286e0c8c9b639113c3421a
dfs28 <danwone1@googlemail.com>
2024-04-26 10:38:39 +0100

    fixed issue with chosing slices for patients

--- scripts/Processing/Pandas_to_np.py
+++ scripts/Processing/Pandas_to_np.py
@@ -12,6 +12,20 @@ import scipy
 from progress.bar import Bar
 import sys
 from functools import reduce
+import argparse
+from tqdm import tqdm
+
+#Make it so you can run this from the command line
+parser = argparse.ArgumentParser(description="Allow running of Pandas_to_np with different input parameters",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("-l", "--length", default=180, type=int, help="Length of input window in minutes")
+parser.add_argument("-z", "--no_z", default=False, type=bool, help="Whether or not to use z-scores")
+args = vars(parser.parse_args())
+
+no_z = args['no_z']
+input_length = args['length']
+
+print(f'Running with {round(input_length/60)}h time input window, no z-scores: {no_z}')
 
 #In reality import the big flowsheet to use thing
 print('Starting run: ', datetime.now().strftime("%H:%M:%S"))
@@ -23,12 +37,10 @@ died = {'N': 0, 'Y': 1}
 flowsheet['died'] = flowsheet['died'].replace(died)
 sex = {'F': 0, 'M': 1, 'I': 2}
 flowsheet['sex'] = flowsheet['sex'].replace(sex)
-ethnicity = {j:i[0] for i, j in np.ndenumerate(flowsheet['ethnicity'].unique())}
-flowsheet['ethnicity'] = flowsheet['ethnicity'].replace(ethnicity)
 print('Correction to float applied: ', datetime.now().strftime("%H:%M:%S"))
 
 #Make 3d training array
-def make_3d_array(array, length, all_cols, point_cols, series_cols, percentile = True):
+def make_3d_array(array, length, all_cols, point_cols, series_cols, percentile = True, normalise = True):
     """
     Function to make an pandas into a 3d np.array of slices and stack them \n
     Takes a dataframe, slices it by the length specified 
@@ -53,52 +65,50 @@ def make_3d_array(array, length, all_cols, point_cols, series_cols, percentile =
     bar = Bar('Normalising data', max=array_use.shape[1])
 
     #Make this an np array with no nans
-    for i in range(array_use.shape[1]):
-        nas = np.isnan(array_use[:, i])
-        if sum(nas == False) < 1:
-            print(i)
-            continue
+    if normalise:
+        for i in range(array_use.shape[1]):
+            nas = np.isnan(array_use[:, i])
+            if any(nas):
+                print(i)
+                continue
 
-        #Scale to percentile
-        if percentile:
-            array_use[nas == False, i] = stats.rankdata(array_use[nas == False, i])
+            #Scale to percentile
+            if percentile:
+                array_use[nas == False, i] = stats.rankdata(array_use[nas == False, i])
 
-        min = np.min(array_use[nas == False, i])
-        max = np.max(array_use[nas == False, i])
-        array_use[nas, i] = min
+            min = np.min(array_use[nas == False, i])
+            max = np.max(array_use[nas == False, i])
+            array_use[nas, i] = min
 
-        #Now normalise 0-1
-        array_use[:, i] -= min
-        if not (max - min) == 0:
-            array_use[:, i] /= (max - min)
+            #Now normalise 0-1
+            array_use[:, i] -= min
+            if not (max - min) == 0:
+                array_use[:, i] /= (max - min)
 
-        bar.next()
-    bar.finish()
-
-
+            bar.next()
+        bar.finish()
         
     shape = array_use.shape
     z_dim = math.floor(np.max(shape)/length)
     
     #Get the slices to use
-    to_use = list()
+    to_use = np.array([], dtype = int)
     unique_patients = array['project_id'].unique()
     slicesPerPatient = np.zeros(len(unique_patients))
-
-    bar = Bar('Getting useable slices', max=z_dim)
-    for i in range(z_dim):
-        start_position = i*length
-        end_position = (i + 1)*length
+    
+    bar = Bar('Getting useable slices', max=len(unique_patients))
+    for location, pt in enumerate(unique_patients):
         
-        #Skip if more than one patient
-        patients = array.loc[range(start_position, end_position), 'project_id'].unique()
-        if patients.shape[0] == 1:
-            to_use.append(i)
+        #Get the positions for each patient
+        pt_locs = np.where(array.project_id == pt)[0]
+        assert len(np.unique(array.project_id[pt_locs])) <= 1, 'There should only be one patient per slice'
         
-            #Record number of slices per patient (in order)
-            patient_loc = unique_patients == patients[0]
-            slicesPerPatient[patient_loc] += 1
-
+        #Get the number of slices per patient
+        pt_slices = int(np.floor(len(array.project_id[pt_locs])/length))
+        slicesPerPatient[location] = pt_slices
+        
+        #Slot in the locations of the patients
+        to_use = np.concatenate([to_use, pt_locs[[i*180 for i in range(int(pt_slices))]]])
         bar.next()
     bar.finish()
 
@@ -115,7 +125,7 @@ def make_3d_array(array, length, all_cols, point_cols, series_cols, percentile =
     p_values = np.empty((len(to_use), len(i_series_cols)))
 
     #Outcomes
-    outcomes = np.zeros((len(to_use), 13))
+    outcomes = np.zeros((len(to_use), 14))
     pt_slices = list()
 
     bar = Bar('Slicing and outcomes', max=len(to_use))
@@ -139,7 +149,7 @@ def make_3d_array(array, length, all_cols, point_cols, series_cols, percentile =
         #Time to death as triplicate value - probably can't assume that death is within this admission?
         if array.loc[end_position, 'time_to_death'] <= 2/365:
             outcomes[position, 2] = 1
-        elif array.loc[end_position, 'died'] == 'Y':
+        elif array.loc[end_position, 'died'] == 1:
             outcomes[position, 3] = 1
         else: 
             outcomes[position, 4] = 1
@@ -164,39 +174,68 @@ def make_3d_array(array, length, all_cols, point_cols, series_cols, percentile =
         pt_locs = np.where(array['project_id'] == project_id)
 
         #Get all slots from end of slice to end of patient
-        next_slices = np.intersect1d(pt_locs[0], range(end_position, array.shape[0]))
-        all_pSOFA_cardio = array.loc[next_slices, 'pSOFA_cardio']
+        next_slices = np.setdiff1d(pt_locs[0], range(start_position, end_position)) #Runs much faster - should speed everything up
+        all_pSOFA_cardio = array.loc[next_slices, 'pSOFA_cardio'] #These should probably be named next_pSOFA/next_lactate etc
         all_lactate = array.loc[next_slices, 'Lactate']
+        all_ECMO = array.loc[next_slices, 'ECMO']
+        all_pSOFA = array.loc[next_slices, 'pSOFA']
         
-        #Get maximum PEWS from current slice
+        #Get maximum pSOFA from current slice
         max_pSOFA_cardio = np.max(array.loc[start_position:end_position, 'pSOFA_cardio'])
+        max_pSOFA = np.max(array.loc[start_position:end_position, 'pSOFA'])
         max_lactate = np.max(array.loc[start_position:end_position, 'Lactate'])
+        max_ECMO = np.max(array.loc[start_position:end_position, 'ECMO'])
+        
 
-        #Get all PEWS deterioration
+        #Get all deterioration - now including ECMO
         worse_pSOFA_cardio = all_pSOFA_cardio > max_pSOFA_cardio
         worse_lactate = np.logical_and(all_lactate > max_lactate, all_lactate > 2)
-        all_worse = reduce(np.logical_or, (worse_pSOFA_cardio, worse_lactate))
+        worse_ECMO = all_ECMO > max_ECMO
+        all_worse = reduce(np.logical_or, (worse_pSOFA_cardio, worse_lactate, worse_ECMO))
         next_dates = all_dates[all_worse.index]
         worse_date = next_dates[all_worse]
-
-        if len(worse_date) > 0 or array.loc[end_position, 'time_to_death'] <= 1/365:
+        
+        #Just do for pSOFA
+        worse_pSOFA = all_pSOFA > max_pSOFA
+        next_dates_pSOFA = all_dates[worse_pSOFA.index]
+        worse_date_pSOFA = next_dates_pSOFA[worse_pSOFA]
+        
+        #Just work out time to deterioration
+        if len(worse_date) > 0:
+            time_to_deteriorate = worse_date[worse_date.index[0]] - end_of_section
             
-            #Need this clause otherwsie time_to_deteriorate messed up if low next pews but dies
-            if len(worse_date) > 0:
-                #As above with discharge date
-                time_to_deteriorate = worse_date[worse_date.index[0]] - end_of_section
-
-            else:
-                #Just set time to PEWS to be how long until they die - have to use pd.Timedelta as np.timedelta64 doesn't accept floats
-                time_to_deteriorate = pd.Timedelta(array.loc[end_position, 'time_to_death']*365, 'D') 
+            #Converting to hours allows easier comparisons later on (and we can use this for the standalone ttd value)
+            hours_to_deteriorate = time_to_deteriorate.total_seconds()/3600
+        else:
+            #100 days in hours
+            hours_to_deteriorate = 2400
+        
+        #Time to worse pSOFA
+        if len(worse_date_pSOFA) > 0:
+            time_to_worse_pSOFA = worse_date_pSOFA[worse_date_pSOFA.index[0]] - end_of_section
+            hours_to_worse_pSOFA = time_to_worse_pSOFA.total_seconds()/3600
+        else:
+            hours_to_worse_pSOFA = 2400
+        
+        #Work out time to death, if less than to deteriorate mark as deteriorated
+        if np.isnan(array.time_to_death[end_position]) == False:
+            time_to_death = pd.Timedelta(array.loc[end_position, 'time_to_death']*365, 'D') 
+            hours_to_death = time_to_death.total_seconds()/3600
+            time_to_deteriorate = np.min([hours_to_death, hours_to_deteriorate])
+            time_to_worse_pSOFA = np.min([hours_to_death, hours_to_worse_pSOFA])
+        else:
+            time_to_deteriorate = hours_to_deteriorate
+            time_to_worse_pSOFA = hours_to_worse_pSOFA
             
-            #Currently setting cutoffs to <6h, 6-24h, >24h
-            if time_to_deteriorate < np.timedelta64(6, 'h') or array.loc[end_position, 'time_to_death'] <= 0.25/365:
-                outcomes[position, 8] = 1
-            elif time_to_deteriorate < np.timedelta64(1, 'D') or array.loc[end_position, 'time_to_death'] <= 1/365:
-                outcomes[position, 9] = 1
-            else:
-                outcomes[position, 10] = 1
+        #Deterioration in hours
+        outcomes[position, 12] = time_to_deteriorate
+        outcomes[position, 13] = time_to_worse_pSOFA
+            
+        #Currently setting cutoffs to <6h, 6-24h, >24h
+        if time_to_deteriorate < 6 or array.loc[end_position, 'time_to_death'] <= 0.25/365:
+            outcomes[position, 8] = 1
+        elif time_to_deteriorate < 24 or array.loc[end_position, 'time_to_death'] <= 1/365:
+            outcomes[position, 9] = 1
         else:
             outcomes[position, 10] = 1
 
@@ -205,14 +244,6 @@ def make_3d_array(array, length, all_cols, point_cols, series_cols, percentile =
             outcomes[position, 11] = 70
         else:
             outcomes[position, 11] = array.loc[end_position, 'time_to_death']
-
-        #Time to deterioration as outcome (if NA then 100 days to deterioration)
-        if pd.isnull(time_to_deteriorate):
-                #100 days in hours
-                outcomes[position, 12] = 2400
-        else:
-                #Deterioration in hours
-                outcomes[position, 12] = time_to_deteriorate.seconds/3600
 
         ##Now get pointwise variables - could do median and mad depending on how scale to percentile looks?
         means = [np.mean(temp_array[:, i]) for i in range(np.shape(temp_array)[1])]
@@ -248,34 +279,70 @@ def make_3d_array(array, length, all_cols, point_cols, series_cols, percentile =
     return array_3d, array_2d, array_characteristics, splines, outcomes, slopes, intercepts, r_values, p_values, std_errs, slicesPerPatient, all_used_cols, j_point_cols, j_series_cols
 
 #Note that HCT. is HCT#
-point_cols = ['ALT', 'Albumin', 'AlkPhos','AST', 'Aspartate', 'Amylase', 'APTT', 'Anion_gap', 'Base_excess', 'Basophils', 'Bicarb',
-            'pH', 'Blood_culture',  'Cr', 'CRP', 'Ca2.', 'Cl', 'Eosinophils', 'FHHb', 'FMetHb', 'FO2Hb', 'Glucose', 'HCT.', 'HCT', 'INR',
-            'Lactate', 'Lymphs', 'Mg', 'Monocytes', 'Neuts', 'P50', 'PaCO2', 'PcCO2', 'PmCO2', 'PaO2', 'PcO2', 'PmO2', 'PO2', 
-            'PvCO2', 'PcO2.1', 'ScvO2', 'Phos', 'Plts', 'K.', 'PT', 'Retics', 'Na.', 'TT', 'Bili', 'WCC', 'Strong_ion_gap', 'Age_yrs', 'sex', 
-            'ethnicity', 'Weight_z_scores', 'Height_z_scores']
- 
 
-series_cols =  ['Ventilation', 'HFO', 'IPAP', 'EPAP', 'Tracheostomy', 'ETCO2', 'FiO2', 'O2Flow', 'O2Flow.kg', 'Ventilation_L_min', 'Ventilation.ml.',
-                'MeanAirwayPressure', 'Ventilation_missing', 'O2Flow.kg_missing', 'IPAP_missing', 'EPAP_missing', 'FiO2_missing', 'HFO_missing',
-                'Tracheostomy_missing', 'Ventilation.ml._missing', 'MeanAirwayPressure_missing', 'ETCO2_missing', 'SBP_zscore', 'DBP_zscore', 
-                'MAP_zscore', 'SysBP_missing', 'DiaBP_missing', 'MAP_missing', 'HR_zscore', 'HR_missing', 'Comfort.Alertness', 'Comfort.BP', 'Comfort.Calmness',
-                'Comfort', 'Comfort.HR', 'Comfort.Resp', 'AVPU', 'GCS_V', 'GCS_E', 'GCS_M', 'GCS', 'GCS_missing', 'AVPU_missing', 'CRT', 'CRT_missing',
-                'SpO2', 'SpO2_missing', 'interpolated_ht_m', 'ECMO', 'ECMO_missing', 'Inotropes_mcg_kg_min', 'EquivAVP_mcg_kg_min', 'EquivDopamine_mcg_kg_min',
-                'Norad_adrenaline_mcg_kg_min', 'EquivMilrinone_mcg_kg_min', 'Inotropes_missing', 'RR_zscore', 'RR_missing',  'AVP_missing', 'Dopamine_missing',
-                'Norad_adrenaline_missing', 'Milrinone_missing',
-                'SF_ratio', 'pSOFA_resp', 'pSOFA_cardio', 'pSOFA_plt', 'pSOFA_bili', 'pSOFA_GCS', 'pSOFA_Cr', 'pSOFA',
-                'dialysis', 'dialysis_missing', 'Temp', 'Temp_missing', 'Urine_output', 'Urine_output_kg', 'Urine_output_missing_kg', 'PEWS']
+if args['no_z'] == False:
+    point_cols = ['ALT', 'Albumin', 'AlkPhos','AST', 'Aspartate', 'Amylase', 'APTT', 'Anion_gap', 'Base_excess', 'Basophils', 'Bicarb',
+                'pH', 'Blood_culture',  'Cr', 'CRP', 'Ca2.', 'Cl', 'Eosinophils', 'FHHb', 'FMetHb', 'FO2Hb', 'Glucose', 'HCT.', 'HCT', 'INR',
+                'Lactate', 'Lymphs', 'Mg', 'Monocytes', 'Neuts', 'P50', 'PaCO2', 'PcCO2', 'PmCO2', 'PaO2', 'PcO2', 'PmO2', 'PO2', 
+                'PvCO2', 'PcO2.1', 'ScvO2', 'Phos', 'Plts', 'K.', 'PT', 'Retics', 'Na.', 'TT', 'Bili', 'WCC', 'Strong_ion_gap', 'Age_yrs', 'sex', 
+                #'ethnicity', 
+                'Weight_z_scores', 'Height_z_scores'] #?Take out ethinicity?
+    
 
-other_cols = ['time_to_death', 'died']
+    series_cols =  ['Ventilation', 'HFO', 'IPAP', 'EPAP', 'Tracheostomy', 'ETCO2', 'FiO2', 'O2Flow', 'O2Flow.kg', 'Ventilation_L_min', 'Ventilation.ml.',
+                    'MeanAirwayPressure', 'Ventilation_missing', 'O2Flow.kg_missing', 'IPAP_missing', 'EPAP_missing', 'FiO2_missing', 'HFO_missing',
+                    'Tracheostomy_missing', 'Ventilation.ml._missing', 'MeanAirwayPressure_missing', 'ETCO2_missing', 'SBP_zscore', 'DBP_zscore', 
+                    'MAP_zscore', 'SysBP_missing', 'DiaBP_missing', 'MAP_missing', 'HR_zscore', 'HR_missing', 'Comfort.Alertness', 'Comfort.BP', 'Comfort.Calmness',
+                    'Comfort', 'Comfort.HR', 'Comfort.Resp', 'AVPU', 'GCS_V', 'GCS_E', 'GCS_M', 'GCS', 'GCS_missing', 'AVPU_missing', 'CRT', 'CRT_missing',
+                    'SpO2', 'SpO2_missing', 'interpolated_ht_m', 'ECMO', 'ECMO_missing', 'Inotropes_mcg_kg_min', 'EquivAVP_mcg_kg_min', 'EquivDopamine_mcg_kg_min',
+                    'Norad_adrenaline_mcg_kg_min', 'EquivMilrinone_mcg_kg_min', 'Inotropes_missing', 'RR_zscore', 'RR_missing',  'AVP_missing', 'Dopamine_missing',
+                    'Norad_adrenaline_missing', 'Milrinone_missing',
+                    'SF_ratio', 'pSOFA_resp', 'pSOFA_cardio', 'pSOFA_plt', 'pSOFA_bili', 'pSOFA_GCS', 'pSOFA_Cr', 'pSOFA',
+                    'dialysis', 'dialysis_missing', 'Temp', 'Temp_missing', 'Urine_output', 'Urine_output_kg', 'Urine_output_missing_kg', 'PEWS']
+
+    other_cols = ['time_to_death', 'died']
+else:
+    #Note that HCT. is HCT#
+    point_cols = ['ALT', 'Albumin', 'AlkPhos','AST', 'Aspartate', 'Amylase', 'APTT', 'Anion_gap', 'Base_excess', 'Basophils', 'Bicarb',
+                'pH', 'Blood_culture', 'Cr', 'CRP', 'Ca2.', 'Cl', 'Eosinophils', 'FHHb', 'FMetHb', 'FO2Hb', 'Glucose', 'HCT.', 'HCT', 'INR',
+                'Lactate', 'Lymphs', 'Mg', 'Monocytes', 'Neuts', 'P50', 'PaCO2', 'PcCO2', 'PmCO2', 'PaO2', 'PcO2', 'PmO2', 'PO2', 
+                'PvCO2', 'PcO2.1', 'ScvO2', 'Phos', 'Plts', 'K.', 'PT', 'Retics', 'Na.', 'TT', 'Bili', 'WCC', 'Strong_ion_gap', 'Age_yrs', 'sex', 
+                'interpolated_wt_kg', 'interpolated_ht_m']
+
+
+    series_cols =  ['Ventilation', 'HFO', 'IPAP', 'EPAP', 'Tracheostomy', 'ETCO2', 'FiO2', 'O2Flow', 'Ventilation_L_min', 'Ventilation.ml.',
+                    'MeanAirwayPressure', 'Ventilation_missing', 'O2Flow.kg_missing', 'IPAP_missing', 'EPAP_missing', 'FiO2_missing', 'HFO_missing',
+                    'Tracheostomy_missing', 'Ventilation.ml._missing', 'MeanAirwayPressure_missing', 'ETCO2_missing', 'SysBP', 'DiaBP', 
+                    'MAP', 'SysBP_missing', 'DiaBP_missing', 'MAP_missing', 'HR', 'HR_missing', 'Comfort.Alertness', 'Comfort.BP', 'Comfort.Calmness',
+                    'Comfort', 'Comfort.HR', 'Comfort.Resp', 'AVPU', 'GCS_V', 'GCS_E', 'GCS_M', 'GCS', 'GCS_missing', 'AVPU_missing', 'CRT', 'CRT_missing',
+                    'SpO2', 'SpO2_missing', 'ECMO', 'ECMO_missing', 'Inotropes_mcg_kg_min', 'EquivAVP_mcg_kg_min', 'EquivDopamine_mcg_kg_min',
+                    'Norad_adrenaline_mcg_kg_min', 'EquivMilrinone_mcg_kg_min', 'Inotropes_missing', 'RR', 'RR_missing', 'AVP_missing', 'Dopamine_missing',
+                    'Norad_adrenaline_missing', 'Milrinone_missing',
+                    'SF_ratio', 'pSOFA_resp', 'pSOFA_cardio', 'pSOFA_plt', 'pSOFA_bili', 'pSOFA_GCS', 'pSOFA_Cr', 'pSOFA',
+                    'dialysis', 'dialysis_missing', 'Temp', 'Temp_missing', 'Urine_output', 'Urine_output_missing_kg', 'PEWS']
+
+    other_cols = ['time_to_death', 'died']
 
 all_cols = point_cols + series_cols + other_cols
 
 #Now run it
-array3d, array2d, array_characteristics, splines, outcomes, slopes, intercepts, r_values, p_values, std_errs, slicesPerPatient, all_used_cols, j_point_cols, j_series_cols = make_3d_array(flowsheet, 180, all_cols, point_cols, series_cols)
-print('Slicing done: ', datetime.now().strftime("%H:%M:%S"))
-np.savez('/store/DAMTP/dfs28/PICU_data/np_arrays_pSOFA.npz', d3 = array3d, d2 = array2d, chars = array_characteristics, splines = splines, slopes = slopes, intercepts =  intercepts, r_values = r_values, p_values = p_values, std_errs = std_errs,
+file_start = '/store/DAMTP/dfs28/PICU_data/np_arrays'
+
+if no_z == False:
+    file_name = file_start + '_pSOFA'
+else:
+    file_name = file_start + '_no_zscore_pSOFA'
+
+if input_length == 180:
+    file_name = file_name + '.npz'
+else:
+    file_name = file_name + f'_{round(input_length/60)}h.npz'
+
+array3d, array2d, array_characteristics, splines, outcomes, slopes, intercepts, r_values, p_values, std_errs, slicesPerPatient, all_used_cols, j_point_cols, j_series_cols = make_3d_array(flowsheet, input_length, all_cols, point_cols, series_cols)
+print(f'Slicing done with length {input_length}: ', datetime.now().strftime("%H:%M:%S"))
+np.savez(file_name, d3 = array3d, d2 = array2d, chars = array_characteristics, splines = splines, slopes = slopes, intercepts =  intercepts, r_values = r_values, p_values = p_values, std_errs = std_errs,
          outcomes = outcomes, per_pt = slicesPerPatient, all_used = np.array(all_used_cols), point_cols = np.array(j_point_cols), series_cols = np.array(j_series_cols))
-print('Saved: ', datetime.now().strftime("%H:%M:%S"))
+print(f'Saved with length {input_length}: ', datetime.now().strftime("%H:%M:%S"))
 
 #Plot PEWS to decide where to set the cutoff
 #Need to also work out how well PEWS itself performs in predicting outcomes
